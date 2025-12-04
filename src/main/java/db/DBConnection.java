481ed881585999ebae895dafd87200bc01ce172a
Lior-Karayev <liork03@gmail.com>
2025-12-04 22:49:12 +0200

    Added config.properties file

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -1,8 +1,11 @@
 package db;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/expense_splitter";
@@ -10,6 +13,21 @@ public class DBConnection {
     private static final String PASSWORD = "L0r1@20803";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        Properties props = new Properties();
+
+        try(InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if(input == null) {
+                throw new RuntimeException("Sorry, unable to find config.properties");
+            }
+
+            props.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading database configuration", e);
+        }
+        return DriverManager.getConnection(
+                props.getProperty("db.url"),
+                props.getProperty("db.user"),
+                props.getProperty("db.password")
+        );
     }
 }
